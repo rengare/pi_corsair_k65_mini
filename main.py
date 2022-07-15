d397ae2801b56347f12d231b4d0c08fce8be3c64
ggorecki <gorecki.gracjan@gmail.com>
2022-07-15 09:54:19 +0200

    changes

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ print("Starting")
 keyboard = KMKKeyboard()
 
 # rgb
-#class AnimationModes:
+# class AnimationModes:
 #    OFF = 0
 #    STATIC = 1
 #    STATIC_STANDBY = 2
@@ -32,7 +32,7 @@ rgb_ext = RGB(
     val_limit=100,
     val_default=25,
     animation_mode=AnimationModes.BREATHING_RAINBOW,
-    animation_speed = 2
+    animation_speed=2
 )
 keyboard.extensions.append(rgb_ext)
 
@@ -68,40 +68,40 @@ XX = KC.NO
 NONE = XX
 TO_CHECK = XX
 ___ = XX
-TRANS = KC.TRANSPARENT
+_x_ = KC.TRANSPARENT
 
-# Layers 
-L0 = KC.TO(0)	
+# Layers
+L0 = KC.TO(0)
 L1 = KC.TT(1)
 L2 = KC.TT(2)
 
 # Settings
 keyboard.tap_time = 250
-keyboard.debug_enabled = False 
+keyboard.debug_enabled = False
 
 # keymap
 keyboard.keymap = [
     [
         ESC, KC.N1, KC.N2, KC.N3, KC.N4, KC.N5, KC.N6, KC.N7, KC.N8, KC.N9, KC.N0, KC.MINUS, KC.EQUAL, KC.BACKSPACE,
-        TAB, KC.Q, KC.W, KC.E, KC.R, KC.T, KC.Y, KC.U, KC.I, KC.O, KC.P, KC.LBRACKET, KC.RBRACKET, ENTER, 
-        SUPER, KC.A, KC.S, KC.D, KC.F, KC.G, KC.H, KC.J, KC.K, KC.L, KC.SEMICOLON, KC.QUOTE, KC.SLASH,
-        ___, SHIFT, KC.ZKHK, KC.Z, KC.X, KC.C, KC.V, KC.B, KC.N, KC.M, KC.COMMA, KC.DOT, KC.BSLASH, KC.RSHIFT,
-        ___, LCTRL, ___, SUPER, KC.LALT,  ___, ___, KC.SPACE,  ___, ___, ___, RALT, L1, L2, RCTRL, 
+        TAB, KC.Q, KC.W, KC.E, KC.R, KC.T, KC.Y, KC.U, KC.I, KC.O, KC.P, KC.LBRACKET, KC.RBRACKET, ENTER,
+        SUPER, KC.A, KC.S, KC.D, KC.F, KC.G, KC.H, KC.J, KC.K, KC.L, KC.SEMICOLON, KC.QUOTE, KC.BSLASH,
+        ___, SHIFT, KC.ZKHK, KC.Z, KC.X, KC.C, KC.V, KC.B, KC.N, KC.M, KC.COMMA, KC.DOT, KC.SLASH, KC.RSHIFT,
+        ___, LCTRL, ___, SUPER, KC.LALT,  ___, ___, KC.SPACE,  ___, ___, ___, RALT, L1, L2, RCTRL,
     ],
     [
-        TRANS, KC.F1, KC.F2, KC.F3, KC.F4, KC.F5, KC.F6, KC.F7, KC.F8, KC.F9, KC.F10, KC.F11, KC.F12, TRANS, 
-        TRANS, KC.RGB_ANI, KC.RGB_AND, KC.E, KC.R, KC.T, KC.PGUP, KC.UP, KC.PGDN, DEL, KC.P, KC.LBRACKET, KC.RBRACKET, ENTER, 
-        TRANS, KC.A, KC.S, KC.D, KC.F, KC.G, KC.LEFT, KC.DOWN, KC.RIGHT, KC.L, KC.SEMICOLON, KC.QUOTE, KC.SLASH,
-        ___, TRANS, TRANS, KC.Z, KC.X, KC.C, KC.V, KC.B, KC.HOME, KC.END, TRANS, TRANS, KC.BSLASH, KC.RSHIFT,
-        ___, TRANS, ___, TRANS, TRANS,  ___, ___, TRANS,  ___, ___, ___, TRANS, L0, L2, TRANS, 
+        _x_, KC.F1, KC.F2, KC.F3, KC.F4, KC.F5, KC.F6, KC.F7, KC.F8, KC.F9, KC.F10, KC.F11, KC.F12, _x_,
+        _x_, KC.RGB_ANI, KC.RGB_AND, _x_, _x_, _x_, KC.PGUP, KC.UP, KC.PGDN, DEL, _x_, _x_, _x_, _x_,
+        _x_, _x_, _x_, _x_, _x_, _x_, KC.LEFT, KC.DOWN, KC.RIGHT, _x_, _x_, _x_, _x_,
+        ___, _x_, _x_, _x_, _x_, _x_, _x_, _x_, KC.HOME, KC.END, _x_, _x_, _x_, _x_,
+        ___, _x_, ___, _x_, _x_,  ___, ___, _x_,  ___, ___, ___, _x_, L0, L2, _x_,
 
     ],
     [
-        KC.RGB_TOG, KC.RGB_MODE_PLAIN, KC.RGB_MODE_BREATHE, KC.RGB_MODE_RAINBOW, KC.RGB_MODE_BREATHE_RAINBOW, KC.RGB_MODE_KNIGHT, KC.F6, KC.F7, KC.F8, KC.F9, KC.F10, KC.F11, KC.F12, TRANS, 
-        TRANS, KC.Q, KC.W, KC.E, KC.R, KC.T, KC.PGUP, KC.UP, KC.PGDN, KC.O, KC.P, KC.LBRACKET, KC.RBRACKET, ENTER, 
-        TRANS, KC.A, KC.S, KC.D, KC.F, KC.G, KC.LEFT, KC.DOWN, KC.RIGHT, KC.L, KC.SEMICOLON, KC.QUOTE, KC.SLASH,
-        ___, TRANS, TRANS, KC.Z, KC.X, KC.C, KC.V, KC.B, KC.HOME, KC.END, TRANS, TRANS, KC.BSLASH, KC.RSHIFT,
-        ___, TRANS, ___, TRANS, TRANS,  ___, ___, TRANS,  ___, ___, ___, TRANS, L0, L1, TRANS, 
+        KC.RGB_TOG, KC.RGB_MODE_PLAIN, KC.RGB_MODE_BREATHE, KC.RGB_MODE_RAINBOW, KC.RGB_MODE_BREATHE_RAINBOW, KC.RGB_MODE_KNIGHT, KC.F6, KC.F7, KC.F8, KC.F9, KC.F10, KC.F11, KC.F12, _x_,
+        _x_, _x_, _x_, _x_, _x_, _x_, KC.PGUP, KC.UP, KC.PGDN, _x_, _x_, _x_, _x_, _x_,
+        _x_, _x_, _x_, _x_, _x_, _x_, KC.LEFT, KC.DOWN, KC.RIGHT, _x_, _x_, _x_, _x_,
+        ___, _x_, _x_, _x_, _x_, _x_, _x_, _x_, KC.HOME, KC.END, _x_, _x_, _x_, _x_,
+        ___, _x_, ___, _x_, _x_,  ___, ___, _x_,  ___, ___, ___, _x_, L0, L1, _x_,
     ]
 ]
 
